1b690360e270b34f01b1d918b2e8090d2bef1207
haadn <hamza.hadj-tayeb@epitech.eu>
2022-04-08 17:19:02 +0200

    [ADD] rendu

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,14 +1,60 @@
 import "./App.css";
-import { Table } from "./components/Table";
 import { Sidebar } from "./components/Sidebar";
+import { useState } from "react";
 
-function App() {
+type City = {
+  name: string;
+  country?: string;
+  subcountry?: string;
+  geonameid?: Number;
+};
+
+export const App = () => {
+  const [cities, setCities] = useState<City[] | null>(null);
+
+
+  const mafonction = () => {
+    fetch("http://localhost:3001/api/cities")
+      .then((response) => response.json())
+      .then(setCities);
+  }
+
+  const cityFilter = (country: string) => {
+    fetch("http://localhost:3001/api/cities?country=" + country)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        setCities(res)
+      });
+  }
+
+  const geoName = (geonameid: Number | undefined) => {
+    window.open("https://www.geonames.org/" + geonameid);
+  }
+
   return (
     <div className="App">
-      <Sidebar />
-      <Table />
+      <Sidebar func={cityFilter} />
+      <div id="cities-table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th id="City"><u> City</u></th>
+            </tr>
+          </thead>
+          <tbody>
+            <button id="buttonCities" onClick={mafonction} >all cities</button>
+            {cities?.map((city, index) => (
+              <tr key={index}>
+                <td id="buttonGeo" onClick={() => geoName(city.geonameid)}><b>{city.name}</b> [{city.country}] ({city.subcountry})
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/frontend/src/components/Sidebar.tsx
+++ frontend/frontend/src/components/Sidebar.tsx
@@ -1,10 +1,44 @@
+import { useEffect } from "react";
 import "./Sidebar.css";
+import {useState} from "react";
 
-export const Sidebar = () => {
+type Country ={
+  name :string;
+  country? : string;
+  subcountry? : string;
+  geonameid: Number;
+  count: Number;
+}
+
+
+export const Sidebar = (props : any) => {
+  const [countries, setCountries] = useState<Country[] | null>(null);
+
+  useEffect(() =>{
+    fetch("http://localhost:3001/api/countries")
+    .then((response) => response.json())
+    .then(setCountries);
+  }, [])
+
+
   return (
-    <div id="sidebar">
-      <h2>Cities App</h2>
-      <div>TODO...</div>
+    <div id="countries-table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th id="Countries"><u> Countries</u></th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries?.map((country, item) =>(
+            <tr key={item}>
+              <td id='selected' onClick={() => { props.func(country.name)}}>{country.name} ({country.count})</td>  
+          </tr>
+          ))}
+        </tbody>
+
+      </table>
+     
     </div>
   );
 };
